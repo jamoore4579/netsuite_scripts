6ad5f639ae79cac1082b29d357ab125e96e79217
Jason Moore <jmoore@weendeavor.com>
2023-06-16 13:49:12 -0400

    setup item amount script

--- edv_ue_total_item_amounts.js
+++ edv_ue_total_item_amounts.js
@@ -6,25 +6,34 @@
 define(['N/record'],
   function(record) {
     function beforeSubmit(context) {
-      if (context.type === context.UserEventType.CREATE || context.type === context.UserEventType.EDIT) {
-        var newRecord = context.newRecord;
-        var itemCount = newRecord.getLineCount({
-          sublistId: 'item'
-        });
-
-        var totalAmount = 0;
-        for (var i = 0; i < itemCount; i++) {
-          var itemAmount = newRecord.getSublistValue({
-            sublistId: 'item',
-            fieldId: 'amount',
-            line: i
+      try {
+        if (context.type === context.UserEventType.CREATE || context.type === context.UserEventType.EDIT) {
+          var newRecord = context.newRecord;
+          var itemCount = newRecord.getLineCount({
+            sublistId: 'item'
           });
-          totalAmount += itemAmount;
-        }
 
-        newRecord.setValue({
-          fieldId: 'custbody_item_amount_total',
-          value: totalAmount
+          var totalAmount = 0;
+          for (var i = 0; i < itemCount; i++) {
+            var itemAmount = newRecord.getSublistValue({
+              sublistId: 'item',
+              fieldId: 'amount',
+              line: i
+            });
+            if (itemAmount) {
+              totalAmount += parseFloat(itemAmount);
+            }
+          }
+
+          newRecord.setValue({
+            fieldId: 'custbody_item_amount_total',
+            value: totalAmount.toFixed(2) // Rounded to 2 decimal places
+          });
+        }
+      } catch (error) {
+        log.error({
+          title: 'Error calculating total amount',
+          details: error.message
         });
       }
     }
