c12a14e3e043ef43cd9cd4f795738ca69f0dce00
Jason Moore <jmoore@weendeavor.com>
2023-09-20 16:01:57 -0400

    update new files

---
+++ edv_cs_update_project_status_SO.js
@@ -0,0 +1,71 @@
+/**
+ * @NApiVersion 2.x
+ * @NScriptType ClientScript
+ * @NModuleScope SameAccount
+ */
+define(['N/record', 'N/log', 'N/ui/dialog'], function (record, log, dialog) {
+
+    function pageInit(context) {
+        var currentRecord = context.currentRecord;
+
+        // Get the Project ID from the Job field on Sales Order
+        var projectId = currentRecord.getValue({
+            fieldId: 'job'
+        });
+
+        if (projectId) {
+            // Load the associated Project record
+            var project = record.load({
+                type: record.Type.JOB,
+                id: projectId
+            });
+
+            // Get the Project Status
+            var projectStatus = project.getValue({
+                fieldId: 'entitystatus' // Adjust fieldId as per your setup
+            });
+
+            // Update the Customer Project Status field on Sales Order
+            currentRecord.setValue({
+                fieldId: 'custbody_project_status', // Adjust fieldId as per your setup
+                value: projectStatus
+            });
+        }
+    }
+
+    function fieldChanged(context) {
+        if (context.fieldId === 'job') {
+            var currentRecord = context.currentRecord;
+
+            // Get the Project ID from the Job field on Sales Order
+            var projectId = currentRecord.getValue({
+                fieldId: 'job'
+            });
+
+            if (projectId) {
+                // Load the associated Project record
+                var project = record.load({
+                    type: record.Type.JOB,
+                    id: projectId
+                });
+
+                // Get the Project Status
+                var projectStatus = project.getValue({
+                    fieldId: 'entitystatus' // Adjust fieldId as per your setup
+                });
+
+                // Update the Customer Project Status field on Sales Order
+                currentRecord.setValue({
+                    fieldId: 'custbody_project_status', // Adjust fieldId as per your setup
+                    value: projectStatus
+                });
+            }
+        }
+    }
+
+    return {
+        pageInit: pageInit,
+        fieldChanged: fieldChanged
+    };
+
+});
