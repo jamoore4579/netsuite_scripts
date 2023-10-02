411db4a97dc8b78733926ca8abd8a19dcafa19c4
Jason Moore <jmoore@weendeavor.com>
2023-10-02 15:18:29 -0400

    update new files

--- edv_cs_show_quote_field.js
+++ edv_cs_show_quote_field.js
@@ -1,60 +1,27 @@
 /**
- * @NApiVersion 2.x
- * @NScriptType ClientScript
- * @NModuleScope SameAccount
- */
-define(['N/currentRecord', 'N/ui/dialog', 'N/record'], function(currentRecord, dialog, record) {
-    
-    /**
-     * Function to check if the current record is an Estimate record.
-     * @returns {boolean} true if the current record is an Estimate, false otherwise.
-     */
-    function isEstimateRecord() {
-        var recordType = currentRecord.get().type;
-        return (recordType === 'estimate');
-    }
-    
-    /**
-     * Function to make the 'Quote #' field required.
-     */
-    function makeQuoteFieldRequired() {
-        var quoteField = currentRecord.get().getField({
-            fieldId: 'custevent_quote_num'
-        });
-        quoteField.isMandatory = true;
-    }
-    
-    /**
-     * Function to set the 'Quote #' field value with the document number from the Estimate record.
-     */
-    function setQuoteFieldValue() {
-        var estimateRecord = currentRecord.get();
-        var quoteField = estimateRecord.getField({
-            fieldId: 'tranid' // Replace with the actual field ID of the document number in the Estimate record
-        });
-        
-        var taskRecord = currentRecord.get();
-        var taskQuoteField = taskRecord.getField({
-            fieldId: 'custevent_quote_num' // Replace with the actual field ID of the 'Quote #' field in the Task record
-        });
-        
-        if (quoteField && taskQuoteField) {
-            var documentNumber = quoteField.getText(); // Assuming the document number is a text field
-            taskQuoteField.setValue(documentNumber);
-        }
-    }
-    
-    /**
-     * Page Init function
-     */
+* @NApiVersion 2.x
+* @NScriptType ClientScript
+* @NModuleScope SameAccount
+*/
+
+define(['N/record', 'N/ui/message', 'N/log'], function (record, message, log) {
+
     function pageInit(context) {
-        if (context.mode === 'create' && isEstimateRecord()) {
-            makeQuoteFieldRequired();
-            setQuoteFieldValue();
-        }
+        // Get the current record
+        var currentRecord = context.currentRecord;
+
+        // Get the record type
+        var recordType = currentRecord.type;
+
+        // Log the record type to the console
+        log.debug('Record Type', recordType);
+
+        // Hide the custom field 'custevent_quote_num'
+        currentRecord.getField({ fieldId: 'custevent_quote_num' }).isDisplay = false;
     }
-    
+
     return {
         pageInit: pageInit
     };
+
 });
