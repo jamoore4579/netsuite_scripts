50ce4a990c3b7d8eb06efa7cf3755005e2fb46d3
Jason Moore <jmoore@weendeavor.com>
2023-10-10 15:36:08 -0400

    button function was added

--- edv_cs_deal_reg_button_click.js
+++ edv_cs_deal_reg_button_click.js
@@ -4,14 +4,48 @@
  * @NModuleScope SameAccount
  */
 
-// Define the deal function globally
-function deal(clicked) {
-    if (clicked) {
-        alert('Button clicked!');
-    }
-}
+define(['N/record', 'N/ui/dialog'], function (record, dialog) {
+    function createCustomRecord() {
+        try {
+            // Create a new custom record
+            var customRecord = record.create({
+                type: 'customrecord651', // Set the record type to your custom record type
+                isDynamic: true,
+            });
 
-define([], function () {
+            // Set field values as needed
+            customRecord.setValue({
+                fieldId: 'custrecord_vendor', // Replace with the actual field ID
+                value: 'Dell' // Replace with the desired field value
+            });
+
+            customRecord.setValue({
+                fieldId: 'custrecord_cust_poc', // Replace with the actual field ID
+                value: 'Endeavor' // Replace with the desired field value
+            });
+
+            customRecord.setValue({
+                fieldId: 'custrecord_sales_rep', // Replace with the actual field ID
+                value: 'Sara Brents' // Replace with the desired field value
+            });
+
+            // Save the custom record
+            var customRecordId = customRecord.save();
+            
+            // Show a confirmation dialog
+            dialog.alert({
+                title: 'Success',
+                message: 'New custom record created with ID: ' + customRecordId
+            });
+        } catch (e) {
+            // Handle any errors
+            dialog.alert({
+                title: 'Error',
+                message: 'An error occurred: ' + e.message
+            });
+        }
+    }
+
     function pageInit(context) {
         // Retrieve the button element by its ID
         var customButton = document.getElementById('custpage_custom_button');
@@ -19,8 +53,8 @@ define([], function () {
         // Attach a click event listener to the button
         if (customButton) {
             customButton.addEventListener('click', function () {
-                // Call the deal function when the button is clicked
-                deal(true);
+                // Call the createCustomRecord function when the button is clicked
+                createCustomRecord();
             });
         }
     }
