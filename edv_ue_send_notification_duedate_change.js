3505c46ec8f419e30797f590a7589f7150d8105d
Jason Moore <jmoore@weendeavor.com>
2023-10-24 10:08:30 -0400

    updated scripts

--- edv_ue_send_notification_duedate_change.js
+++ edv_ue_send_notification_duedate_change.js
@@ -17,13 +17,18 @@ define(['N/record', 'N/email', 'N/runtime', 'N/format'], function (record, email
         return format.format({ value: date, type: format.Type.DATE });
     }
 
-    function sendDueDateChangeNotification(task, oldDueDate, newDueDate, assignedUser) {
+    function sendDueDateChangeNotification(task, oldDueDate, newDueDate, assignedUser, message) {
         var taskCreator = task.getValue({ fieldId: 'owner' });
         var emailSubject = 'Task Due Date Change Notification';
         var emailBody = 'The due date of a task you created has been changed.\n\n';
         emailBody += 'Original Due Date: ' + oldDueDate + '\n';
         emailBody += 'New Due Date: ' + newDueDate + '\n';
         emailBody += 'Assigned User: ' + assignedUser + '\n';
+
+        // Check if a message exists and add it to the email body
+        if (message) {
+            emailBody += 'Message: ' + message + '\n';
+        }
 
         // Send the email
         email.send({
@@ -39,14 +44,16 @@ define(['N/record', 'N/email', 'N/runtime', 'N/format'], function (record, email
             var newTask = context.newRecord;
             var oldTask = context.oldRecord;
             var fieldIdToMonitor = 'duedate';
+            var messageField = 'message';
 
             if (hasFieldChanged(newTask, oldTask, fieldIdToMonitor)) {
                 var oldDueDate = formatDateWithoutTime(oldTask.getValue({ fieldId: fieldIdToMonitor }));
                 var newDueDate = formatDateWithoutTime(newTask.getValue({ fieldId: fieldIdToMonitor }));
                 var assignedUser = newTask.getText({ fieldId: 'assigned' });
+                var message = newTask.getValue({ fieldId: messageField });
 
                 if (oldDueDate !== newDueDate) {
-                    sendDueDateChangeNotification(newTask, oldDueDate, newDueDate, assignedUser);
+                    sendDueDateChangeNotification(newTask, oldDueDate, newDueDate, assignedUser, message);
                 }
             }
         }
