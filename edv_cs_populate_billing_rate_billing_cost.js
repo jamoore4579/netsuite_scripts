4f49d6786717bf0a02fde8e71ac9557b25ac587f
Jason Moore <jmoore@weendeavor.com>
2023-10-27 09:18:45 -0400

    updated scripts

--- edv_cs_populate_billing_rate_billing_cost.js
+++ edv_cs_populate_billing_rate_billing_cost.js
@@ -7,59 +7,47 @@ define(['N/currentRecord', 'N/record'],
 function (currentRecord, record) {
 
     function pageInit(context) {
-        // Get the current record
-        var currentRecordObj = currentRecord.get();
+        try {
+            // Get the current record
+            var currentRecordObj = currentRecord.get();
+            var billingClass = currentRecordObj.getValue({ fieldId: 'billingclass' });
 
-        // Get the value of the "billingclass" field
-        var billingClass = currentRecordObj.getValue({
-            fieldId: 'billingclass'
-        });
+            // Define an object for billing classes and their corresponding custom record IDs
+            var billingClasses = {
+                1: 1,
+                2: 2,
+                3: 3,
+                4: 4,
+                5: 5,
+                6: 6,
+                7: 7,
+                8: 8
+            };
 
-        // Define an array of billing classes and their corresponding custom record IDs
-        var billingClasses = {
-            1: 1,
-            2: 2,
-            3: 3,
-            4: 4,
-            5: 5,
-            6: 6,
-            7: 7,
-            8: 8
-        };
+            if (billingClasses[billingClass]) {
+                var customRecordId = billingClasses[billingClass];
+                var customRecordType = 'customrecord653';
 
-        if (billingClasses.hasOwnProperty(billingClass)) {
-            var customRecordId = billingClasses[billingClass];
-            var customRecordType = 'customrecord653';
-
-            var customRecordLookup = record.load({
-                type: customRecordType,
-                id: customRecordId,
-                isDynamic: true
-            });
-
-            var billRateValue = customRecordLookup.getValue({
-                fieldId: 'custrecord_tech_bill_rate'
-            });
-
-            var costRateValue = customRecordLookup.getValue({
-                fieldId: 'custrecord_tech_cost_rate'
-            });
-
-            if (billRateValue && costRateValue) {
-                console.log('Billing Class Value is ' + billingClass);
-                console.log('Project Rates - Bill: ' + billRateValue + ' Cost: ' + costRateValue);
-
-                // Set the values for custentity_billing_rate and custentity_cost_rate fields
-                currentRecordObj.setValue({
-                    fieldId: 'custentity_billing_rate',
-                    value: billRateValue
+                var customRecordLookup = record.load({
+                    type: customRecordType,
+                    id: customRecordId,
+                    isDynamic: true
                 });
 
-                currentRecordObj.setValue({
-                    fieldId: 'custentity_cost_rate',
-                    value: costRateValue
-                });
+                var billRateValue = customRecordLookup.getValue({ fieldId: 'custrecord_tech_bill_rate' });
+                var costRateValue = customRecordLookup.getValue({ fieldId: 'custrecord_tech_cost_rate' });
+
+                if (billRateValue && costRateValue) {
+                    console.log('Billing Class Value is ' + billingClass);
+                    console.log('Project Rates - Bill: ' + billRateValue + ' Cost: ' + costRateValue);
+
+                    // Set the values for custentity_billing_rate and custentity_cost_rate fields
+                    currentRecordObj.setValue({ fieldId: 'custentity_billing_rate', value: billRateValue });
+                    currentRecordObj.setValue({ fieldId: 'custentity_cost_rate', value: costRateValue });
+                }
             }
+        } catch (e) {
+            console.error('An error occurred: ' + e.message);
         }
     }
 
