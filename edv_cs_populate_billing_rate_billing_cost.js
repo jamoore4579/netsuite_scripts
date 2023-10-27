f94deda11e72ccf62c97fa7a65b02b202e70d3fa
Jason Moore <jmoore@weendeavor.com>
2023-10-27 09:35:41 -0400

    updated scripts

--- edv_cs_populate_billing_rate_billing_cost.js
+++ edv_cs_populate_billing_rate_billing_cost.js
@@ -4,6 +4,7 @@
  * @NModuleScope SameAccount
  */
 define(['N/currentRecord', 'N/record'],
+
 function (currentRecord, record) {
 
     function pageInit(context) {
@@ -51,7 +52,57 @@ function (currentRecord, record) {
         }
     }
 
+    function fieldChanged(context) {
+
+        if (context.fieldId === 'billingclass') {
+
+            try {
+                var currentRecordObj = currentRecord.get();
+                var billingClass = currentRecordObj.getValue({ fieldId: 'billingclass' });
+
+                var billingClasses = {
+                    1: 1,
+                    2: 2,
+                    3: 3,
+                    4: 4,
+                    5: 5,
+                    6: 6,
+                    7: 7,
+                    8: 8
+                };
+
+                if (billingClasses[billingClass]) {
+                    var customRecordId = billingClasses[billingClass];
+                    var customRecordType = 'customrecord653';
+
+                    var customRecordLookup = record.load({
+                        type: customRecordType,
+                        id: customRecordId,
+                        isDynamic: true
+                    });
+
+                    var billRateValue = customRecordLookup.getValue({ fieldId: 'custrecord_tech_bill_rate' });
+                    var costRateValue = customRecordLookup.getValue({ fieldId: 'custrecord_tech_cost_rate' });
+
+                    if (billRateValue && costRateValue) {
+                        console.log('Billing Class Value is ' + billingClass);
+                        console.log('Project Rates - Bill: ' + billRateValue + ' Cost: ' + costRateValue);
+
+                        currentRecordObj.setValue({ fieldId: 'custentity_billing_rate', value: billRateValue });
+                        currentRecordObj.setValue({ fieldId: 'custentity_cost_rate', value: costRateValue });
+                    }
+                }
+
+            
+            } catch (e) {
+                console.error('An error occurred: ' + e.message);
+            }
+        }
+    }
+
+
     return {
-        pageInit: pageInit
+        pageInit: pageInit,
+        fieldChanged: fieldChanged
     };
 });
