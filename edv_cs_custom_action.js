4d1d2d658af64560708aae7ad0ac4144e6bf4916
Jason Moore <jmoore@weendeavor.com>
2023-11-01 11:02:54 -0400

    updated scripts

--- edv_cs_custom_action.js
+++ edv_cs_custom_action.js
@@ -12,7 +12,8 @@ define(['N/url'], function (url) {
 
     function onClickButton() {
         // Replace 'SUITELET_URL' with the actual URL of your Suitelet.
-        var suiteletURL = 'https://5779703-sb1.app.netsuite.com/app/site/hosting/scriptlet.nl?script=915&deploy=1';
+        var suiteletURL = 'https://5779703-sb1.app.netsuite.com/app/common/custom/custrecordentry.nl?rectype=654&cf=170';
+        // 'https://5779703-sb1.app.netsuite.com/app/site/hosting/scriptlet.nl?script=915&deploy=1'
 
         // Open the Suitelet custom form in a new tab or window.
         window.open(suiteletURL, '_blank');
