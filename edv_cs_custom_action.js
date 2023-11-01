f987972a03f477aa7e9014a9d1b234cb84c284b0
Jason Moore <jmoore@weendeavor.com>
2023-11-01 10:20:01 -0400

    updated scripts

---
+++ edv_cs_custom_action.js
@@ -0,0 +1,25 @@
+/**
+ * @NApiVersion 2.x
+ * @NScriptType ClientScript
+ * @NModuleScope Public
+ */
+
+define(['N/url'], function (url) {
+
+    function pageInit(context) {
+        // This function is called when the page is loaded.
+    }
+
+    function onClickButton() {
+        // Replace 'SUITELET_URL' with the actual URL of your Suitelet.
+        var suiteletURL = 'https://5779703-sb1.app.netsuite.com/app/site/hosting/scriptlet.nl?script=915&deploy=1';
+
+        // Open the Suitelet custom form in a new tab or window.
+        window.open(suiteletURL, '_blank');
+    }
+
+    return {
+        pageInit: pageInit,
+        onClickButton: onClickButton
+    };
+});
---
+++ edv_su_deal_entry_form.js
@@ -0,0 +1,75 @@
+/**
+ * @NApiVersion 2.x
+ * @NScriptType Suitelet
+ * @NModuleScope Public
+ */
+
+define(['N/ui/serverWidget', 'N/currentRecord', 'N/record'], function (serverWidget, currentRecord, record) {
+
+    function onRequest(context) {
+        if (context.request.method === 'GET') {
+            // Create a form
+            var form = serverWidget.createForm({
+                title: 'Custom Record 654 Entry Form'
+            });
+
+            // Add a hidden field for record identification
+            form.addField({
+                id: 'custpage_record_id',
+                type: serverWidget.FieldType.TEXT,
+                label: 'Record ID'
+            }).updateDisplayType({
+                displayType: serverWidget.FieldDisplayType.HIDDEN
+            });
+
+            // Retrieve the related Opportunity field value (replace with the actual field ID)
+            var relatedOpportunity = 'Sample Related Opportunity Value';
+
+            // Add a field to display the related Opportunity field value
+            form.addField({
+                id: 'custpage_related_opportunity',
+                type: serverWidget.FieldType.TEXT,
+                label: 'Related Opportunity'
+            }).defaultValue = relatedOpportunity;
+
+            // Add "Save" and "Cancel" buttons
+            form.addSubmitButton({
+                label: 'Save'
+            });
+
+            // Add a cancel button and associated JavaScript function to close the window
+            form.addButton({
+                id: 'custpage_cancel_button',
+                label: 'Cancel',
+                functionName: 'closeWindow()'
+            });
+
+            // Write the response
+            context.response.writePage(form);
+        } else if (context.request.method === 'POST') {
+            // Handle form submission
+            var customFieldId = 'custrecord_custom_field_id'; // Replace with your custom field internal ID
+
+            // Retrieve the custom field value from the submitted form data
+            var customFieldValue = context.request.parameters[customFieldId];
+
+            // Here, you can use customFieldValue to update or create your custom record
+            // ...
+
+            // Redirect to a success page or back to the custom record
+            context.response.sendRedirect({
+                type: serverWidget.RedirectType.RECORD,
+                identifier: customRecordId // Replace with the correct identifier
+            });
+        }
+    }
+
+    return {
+        onRequest: onRequest
+    };
+});
+
+// JavaScript function to close the window
+function closeWindow() {
+    window.close();
+}
