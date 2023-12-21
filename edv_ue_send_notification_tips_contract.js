e2dee5bde6888092df39cb2296d95bea808c7a37
Jason Moore <jmoore@weendeavor.com>
2023-12-21 09:45:54 -0500

    updated scripts

--- edv_ue_send_notification_tips_contract.js
+++ edv_ue_send_notification_tips_contract.js
@@ -51,38 +51,45 @@ define(['N/log', 'N/email', 'N/record'], function(log, email, record) {
                 details: nationFieldValues
             });
 
-            // Modified condition to check if nationFieldValues is 4, 5, 6, 7, 8, 9, 10, 11, or 12
-            if (nationFieldValues >= 4 && nationFieldValues <= 12) {
+            if (checkNotification = false) {
+                // Modified condition to check if nationFieldValues is 4, 5, 6, 7, 8, 9, 10, 11, or 12
+                if (nationFieldValues >= 4 && nationFieldValues <= 12) {
 
-                var emailBody = 'Notification message for Sales Order:' + transactionId + '\n';
-                emailBody += 'Purchasing Contract: ' + nationFieldText
+                    var emailBody = 'Notification message for Sales Order: ' + transactionId + '\n';
+                    emailBody += 'Purchasing Contract: ' + nationFieldText
 
-                // Send email to user 3578
-                email.send({
-                    author: salesRep, // The internal ID of the user sending the email
-                    recipients: 'jmoore@weendeavor.com', // Replace with the actual email address of user 3578
-                    subject: 'Notification for Transaction ' + transactionId,
-                    body: emailBody
-                });
+                    // Send email to user 3578
+                    email.send({
+                        author: salesRep, // The internal ID of the user sending the email
+                        recipients: 'jmoore@weendeavor.com', // Replace with the actual email address of user 3578
+                        subject: 'Notification for Transaction ' + transactionId,
+                        body: emailBody
+                    });
 
-                log.debug({
-                    title: 'Condition Met',
-                    details: 'Notification was sent.'
-                });
+                    log.debug({
+                        title: 'Condition Met',
+                        details: 'Notification was sent.'
+                    });
 
-                // Mark the notification as sent to avoid sending it again
-                record.submitFields({
-                    type: newRecord.type,
-                    id: newRecord.id,
-                    values: {
-                        custbody_contract_notification: true
-                    }
-                });
+                    // Mark the notification as sent to avoid sending it again
+                    record.submitFields({
+                        type: newRecord.type,
+                        id: newRecord.id,
+                        values: {
+                            custbody_contract_notification: true
+                        }
+                    });
+                } else {
+                    log.debug({
+                        title: 'Condition Not Met',
+                        details: 'Notification should not be sent.'
+                    });
+                }
             } else {
                 log.debug({
-                    title: 'Condition Not Met',
-                    details: 'Notification should not be sent.'
-                });
+                    title: 'Notification Check',
+                    details: 'Notification Already Sent.' 
+                })
             }
         }
 
