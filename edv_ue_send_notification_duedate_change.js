bfa69b6481864799c42f52badb52205c7505cf0e
Jason Moore <jmoore@weendeavor.com>
2024-01-03 11:00:22 -0500

    updated scripts

--- edv_ue_send_notification_duedate_change.js
+++ edv_ue_send_notification_duedate_change.js
@@ -6,7 +6,6 @@
 
 define(['N/record', 'N/email', 'N/runtime', 'N/format'], function (record, email, runtime, format) {
 
-    
     function hasFieldChanged(newRecord, oldRecord, fieldId) {
         var newValue = newRecord.getValue({ fieldId: fieldId });
         var oldValue = oldRecord.getValue({ fieldId: fieldId });
@@ -17,10 +16,12 @@ define(['N/record', 'N/email', 'N/runtime', 'N/format'], function (record, email
         return format.format({ value: date, type: format.Type.DATE });
     }
 
-    function sendDueDateChangeNotification(task, oldDueDate, newDueDate, assignedUser, message) {
+    function sendDueDateChangeNotification(task, taskId, oldDueDate, newDueDate, transaction, assignedUser, message) {
         var taskCreator = task.getValue({ fieldId: 'owner' });
         var emailSubject = 'Task Due Date Change Notification';
         var emailBody = 'The due date of a task you created has been changed.\n\n';
+        emailBody += 'Related Transaction: ' + transaction + '\n';
+        emailBody += 'Related Task: ' + taskId + '\n';
         emailBody += 'Original Due Date: ' + oldDueDate + '\n';
         emailBody += 'New Due Date: ' + newDueDate + '\n';
         emailBody += 'Assigned User: ' + assignedUser + '\n';
@@ -49,11 +50,13 @@ define(['N/record', 'N/email', 'N/runtime', 'N/format'], function (record, email
             if (hasFieldChanged(newTask, oldTask, fieldIdToMonitor)) {
                 var oldDueDate = formatDateWithoutTime(oldTask.getValue({ fieldId: fieldIdToMonitor }));
                 var newDueDate = formatDateWithoutTime(newTask.getValue({ fieldId: fieldIdToMonitor }));
+                var transaction = newTask.getValue({ fieldId: 'transaction' }); // Moved the declaration here
                 var assignedUser = newTask.getText({ fieldId: 'assigned' });
                 var message = newTask.getValue({ fieldId: messageField });
 
                 if (oldDueDate !== newDueDate) {
-                    sendDueDateChangeNotification(newTask, oldDueDate, newDueDate, assignedUser, message);
+                    var taskId = newTask.id; // Retrieving internal ID of the task record
+                    sendDueDateChangeNotification(newTask, taskId, oldDueDate, newDueDate, transaction, assignedUser, message);
                 }
             }
         }
